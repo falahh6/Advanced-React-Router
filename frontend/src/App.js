12bbe792ce9e82cf0e43002668d6e0d7fce54ead
Mohammed Falah <falahsss900@gmail.com>
2023-04-17 13:59:33 +0530

    ADD - New Events Adding functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './Pages/Root';
 import Events, { eventsLoader } from './Pages/Events';
 import Home from './Pages/Home';
-import NewEvents from './Pages/NewEvents';
+import NewEvents, { newEventAction } from './Pages/NewEvents';
 import EventDetails, { loader as eventDetailsLoader} from './Pages/EventDetails';
 import EventRoot from './Pages/EventRoot';
 import EditEvents from './Pages/EditEvents';
@@ -19,11 +19,11 @@ function App() {
         { path : '/', element : <Home /> },
         { path : '/events' , element : <EventRoot />, children : [
           { path : '/events', element : <Events /> , loader : eventsLoader },
-          { path : 'new', element : <NewEvents />},
           { path : ':eID', id : 'event-details', loader : eventDetailsLoader, children : [
-            { index : true, element : <EventDetails />  },
+            { index : true, element : <EventDetails /> , },
             { path : 'edit', element : <EditEvents /> }
-          ]}
+          ]},
+          { path : 'new', element : <NewEvents />, action : newEventAction},
         ] },
       ]
     }
