a4bf9b75fc93d2b4e0217c23792b7bbff5eb603d
Mohammed Falah <falahsss900@gmail.com>
2023-04-17 14:00:31 +0530

    follow up commit

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,31 +1,29 @@
-import {useNavigate } from 'react-router-dom';
+import {Form, useNavigate } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
-  console.log(event);
   const navigate = useNavigate();
   function cancelHandler() {
     navigate('..');
   }
-
   return (
-    <form className={classes.form}>
+    <Form method='post' className={classes.form}>
       <p>
         <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" required defaultValue={event.title} />
+        <input id="title" type="text" name="title" required defaultValue={event ? event.title : ''} />
       </p>
       <p>
         <label htmlFor="image">Image</label>
-        <input id="image" type="url" name="image" required defaultValue={event.image}/>
+        <input id="image" type="url" name="image" required defaultValue={event ? event.image : ''}/>
       </p>
       <p>
         <label htmlFor="date">Date</label>
-        <input id="date" type="date" name="date" required defaultValue={event.date}/>
+        <input id="date" type="date" name="date" required defaultValue={event ? event.date : ''}/>
       </p>
       <p>
         <label htmlFor="description">Description</label>
-        <textarea id="description" name="description" rows="5" required defaultValue={event.description}/>
+        <textarea id="description" name="description" rows="5" required defaultValue={event ? event.description : ''}/>
       </p>
       <div className={classes.actions}>
         <button type="button" onClick={cancelHandler}>
@@ -33,7 +31,7 @@ function EventForm({ method, event }) {
         </button>
         <button>Save</button>
       </div>
-    </form>
+    </Form>
   );
 }
 
